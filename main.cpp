08b36aa3d63d5e031fda08ab5ebf0fe44db09b37
HugDi <113516550+GugDi@users.noreply.github.com>
2023-10-09 19:54:48 +0400

    main.cpp - Создание объекта классов + argc и argv

--- main.cpp
+++ main.cpp
@@ -1,4 +1,9 @@
 #include "widget.h"
 #include <QApplication>
 
-int main() {};
+int main(int argc, char* argv[]) {
+    QApplication app(argc, argv);
+    Widget win;
+    win.show();
+    return app.exec();
+}
