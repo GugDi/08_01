3072603d40ad594f001258b83baa659d9e8da043
HugDi <113516550+GugDi@users.noreply.github.com>
2023-10-09 20:01:05 +0400

    widget.cpp - Widget::Widget - наименование кнопок

--- widget.cpp
+++ widget.cpp
@@ -1,7 +1,12 @@
 #include "widget.h"
 #include <QMessageBox>
 
-Widget::Widget(QWidget* parent) :QWidget(parent) {}
+Widget::Widget(QWidget* parent) :QWidget(parent) {
+    this->setFixedWidth(200);
+    startbtn = new QPushButton("Start");
+    stopbtn = new QPushButton("Stop");
+    pausebtn = new QPushButton("Pause");
+}
 Widget::~Widget() {}
 
 void Widget::starting() {}
